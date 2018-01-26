61e3150c77917825d90b734aeb3aa4308dd070c0
Aaron Wolfcale <aaronwolfcale@Aarons-MacBook-Pro.local>
2018-01-26 14:10:38 -0600

    finished exerceise

---
+++ app/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+angular.module("shroomFinder", ["ngRoute"])
+.config($routeProvider => {
+  $routeProvider
+    .when("/", {
+      templateUrl: "partials/shroom.html",
+      controller: "ShroomCtrl"
+    })
+    .otherwise("/")
+});
